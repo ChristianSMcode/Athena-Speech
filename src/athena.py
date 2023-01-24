c2e3e09a63f64d168da5c9f464aa2d535caac960
Christian David <cris920sdm@hotmail.com>
2023-01-23 19:51:17 -0500

    multi-language implementation

--- src/athena.py
+++ src/athena.py
@@ -2,30 +2,39 @@ import utils.voice_synthesis as synth
 from utils.voice_trigger import listen_activate,recognize_option
 from utils.words_manipulation import compare_words_simil
 import time
+import random
+import json
+from dotenv import load_dotenv
+import os
 
-execution_mode=[
-    'conversación',
-    'media',
-    'command',
-    'assistance',
-    'fileSystem'
-]
+load_dotenv()
+
+LANGUAGE=os.environ['LANGUAGE']
+with open(f'metadata/languages/dictionary_{LANGUAGE}.json', encoding='utf-8') as file:
+    configuration_json=json.load(file)
 
 def main():
     while True:
+        random_number=random.randint(0,2)
         print('Activalo')
-        if listen_activate('actívate'):
-            synth.generate_response_audio('En que modo deseas ejecutar')
-            mode=recognize_option(execution_mode)
-            if mode == 'conversación':
+        if listen_activate(configuration_json['activation_word'],LANGUAGE):
+            
+            synth.generate_response_audio(configuration_json['prompts']['initialization'][random_number])
+            mode=recognize_option(configuration_json['execution_mode'],LANGUAGE)
+
+            if mode == configuration_json['execution_mode'][0]:
                 synth.generate_response_audio('Escuchando en modo conversation.')
-            elif mode == 'media':
+
+            elif mode == configuration_json['execution_mode'][1]:
                 synth.generate_response_audio('Modo no disponible')
-            elif mode == 'command':
+
+            elif mode == configuration_json['execution_mode'][2]:
                 synth.generate_response_audio('Modo no disponible')
-            elif mode == 'assistance':
+
+            elif mode == configuration_json['execution_mode'][3]:
                 synth.generate_response_audio('Modo no disponible')
-            elif mode == 'fileSystem':
+
+            elif mode == configuration_json['execution_mode'][4]:
                 synth.generate_response_audio('Modo no disponible')
             else:
                 synth.generate_response_audio('No entendí, intenta denuveo')
--- src/utils/voice_trigger.py
+++ src/utils/voice_trigger.py
@@ -1,22 +1,23 @@
 import speech_recognition as sr
+import os
 
-def listen_activate(trigger_word):
+def listen_activate(trigger_word,language):
     r= sr.Recognizer()
     with sr.Microphone() as source:
         audio= r.listen(source)
     try:
-        text = r.recognize_google(audio,language="es-ES")
+        text = r.recognize_google(audio,language=language)
         if trigger_word in text.lower():
             return True
     except:
         return False
 
-def recognize_option(opts):
+def recognize_option(opts,language):
     r= sr.Recognizer()
     with sr.Microphone() as source:
         audio= r.listen(source)
     try:
-        text = r.recognize_google(audio,language="es-ES")
+        text = r.recognize_google(audio,language=language)
         for word in opts:
             if word in text.lower():
                 return word
